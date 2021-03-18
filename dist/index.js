ba6f40eab1acd5399aa9dbf49b93ad010b126602
Saifoo Khan <isaifookhan@gmail.com>
2021-03-18 02:46:06 +0100

    graphql + resolver added

--- dist/index.js
+++ dist/index.js
@@ -12,13 +12,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
-const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+require("reflect-metadata");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolver/hello");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
-    const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
-    const post = orm.em.create(Post_1.Post, { title: "my first post" });
-    orm.em.persistAndFlush(post);
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver],
+            validate: false
+        })
+    });
+    apolloServer.applyMiddleware({ app });
+    app.get('/', (_, res) => {
+        res.send("Hello from the server");
+    });
+    app.listen(4000, () => {
+        console.log("started at 4000");
+    });
 });
 main();
 console.log("hi22");
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,31 @@
-import {MikroORM} from "@mikro-orm/core"
-import { Post } from "./entities/Post"
-import mikroConfig from "./mikro-orm.config"
+// import {MikroORM} from "@mikro-orm/core"
+// import mikroConfig from "./mikro-orm.config"
+import express from "express"
+import {ApolloServer} from "apollo-server-express"
+import "reflect-metadata";
+
+import {buildSchema} from "type-graphql"
+import { HelloResolver } from "./resolver/hello"
 
 const main = async () =>{
-    const orm = await MikroORM.init(mikroConfig)
+    // const orm = await MikroORM.init(mikroConfig)
 
-    const post = orm.em.create(Post, {title: "my first post"})
-    orm.em.persistAndFlush(post)
+    const app = express()
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+    })})
+
+    apolloServer.applyMiddleware({app})
+
+    app.get('/', (_, res) =>{
+        res.send("Hello from the server")
+    })
+    app.listen(4000, ()=>{
+        console.log("started at 4000")
+    })
 }
 
 main()
